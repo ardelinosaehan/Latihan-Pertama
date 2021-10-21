7ae472ce431c90a5795f48cd81d0a186113b9dec
unknown <saehanardelino@gmail.com>
2021-10-21 11:10:06 +0700

    Program python1

---
+++ latihan1.py
@@ -0,0 +1,24 @@
+print ("Latihan Pertama")
+print ()
+namalengkap    = input ("nama anda :")
+namapanggilan  = input ("nama panggilan :")
+nim            = input ("nim anda :")
+tempatlahir    = input ("tempat lahir :")
+tanggallahir   = input ("tanggal lahir :")
+telepon        = input ("telepon anda :")
+alamat         = input ("alamat anda :")
+umur           = input ("umur anda :")
+print() 
+print ("Assalamualaikum")
+print()
+print ("Let me introduce my self. my name is",namalengkap,"but you can call me",namapanggilan,"My NIM is",nim,"I was born in",tempatlahir,"and I'm",umur,"years old. I am very glad if you want to invite my house in",alamat,". So, dont forget to call me before with the number",telepon,)
+print ()
+print ("Thank You.")
+
+
+
+
+
+
+
+
